ca5d544b4c65ba86c8123f7df5bcabb61726c62e
Rita Wang <rita.wang@thecarousell.com>
2019-09-29 23:36:50 +0800

    Update button css

--- src/components/button.js
+++ src/components/button.js
@@ -7,25 +7,29 @@ const Button = props => (
 
 const ButtonWrapper = styled.button`
   display: block;
-  border: none;
   text-align: center;
   box-sizing: border-box;
   text-decoration: none;
-  padding: 10px 25px;
+  padding: 8px 16px;
   cursor: pointer;
-  text-transform: uppercase;
-  letter-spacing: 2px;
 
-  background: ${props => props.props.background || "black"};
-  color: ${props => props.props.color || "rgb(255, 255, 255)"};
+  background: ${props => props.props.background || "#FFFFFF"};
+  color: ${props => props.props.color || "#4455BB"};
   font-size: ${props => props.props.fontSize || "15px"};
   font-weight: ${props => props.props.fontWeight || "600"};
-  border-radius: ${props => props.props.radius || "6px"};
+  border-radius: ${props => props.props.radius || "8px"};
+  border-color: ${props => props.props.boderColor || "#4455BB"};
+  border-width: ${props => props.props.boderWidth || "2px"};
   margin-top: ${props => props.props.marginTop};
   margin-bottom: ${props => props.props.marginBottom};
 
   &:hover {
-    box-shadow: inset 0 0 100px 100px rgba(255, 255, 255, 0.25);
+    background-color: #4455BB;
+    color: #ffffff;
+  }
+
+  &:focus {
+    outline: none;
   }
 `
 
