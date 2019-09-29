06489165997c54770067aca315255445d82fecbf
Rita Wang <rita.wang@thecarousell.com>
2019-09-29 21:06:16 +0800

    Move Single case to component Case

---
+++ src/components/case.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import PropTypes from "prop-types"
+import { rhythm } from "../utils/typography"
+import Button from "../components/button"
+
+function Case({post}) {
+    
+    const title = post.frontmatter.title || post.fields.slug
+    return (
+        <div key={post.fields.slug}>
+            <h3
+            style={{
+                marginBottom: rhythm(1 / 4),
+            }}
+            >
+            <Link
+                style={{ boxShadow: `none` }}
+                to={`blog${post.fields.slug}`}
+            >
+                {title}
+            </Link>
+            </h3>
+            <small>{post.frontmatter.date}</small>
+            <p
+            dangerouslySetInnerHTML={{
+                __html: post.frontmatter.description || post.excerpt,
+            }}
+            />
+            <Button marginTop="35px">See case study</Button>
+        </div>
+    )
+}
+
+Case.defaultProps = {
+  post: null
+}
+
+Case.propTypes = {
+  post: PropTypes.object,
+}
+export default Case
--- src/components/section-case.js
+++ src/components/section-case.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { rhythm } from "../utils/typography"
-import { Link, graphql, StaticQuery } from "gatsby"
-import Button from "../components/button"
+import { graphql, StaticQuery } from "gatsby"
+import Case from "../components/case"
 
 function SectionCase() {
 
@@ -10,33 +9,12 @@ function SectionCase() {
             query={postQuery}
             render={data => {
                 const posts = data.allMarkdownRemark.edges
+                
                 return (
                     <div style={{ margin: "20px 0 40px" }}>
                         {posts.map(({ node }) => {
-                            const title = node.frontmatter.title || node.fields.slug
-                            return (
-                            <div key={node.fields.slug}>
-                                <h3
-                                style={{
-                                    marginBottom: rhythm(1 / 4),
-                                }}
-                                >
-                                <Link
-                                    style={{ boxShadow: `none` }}
-                                    to={`blog${node.fields.slug}`}
-                                >
-                                    {title}
-                                </Link>
-                                </h3>
-                                <small>{node.frontmatter.date}</small>
-                                <p
-                                dangerouslySetInnerHTML={{
-                                    __html: node.frontmatter.description || node.excerpt,
-                                }}
-                                />
-                                <Button marginTop="35px">See case study</Button>
-                            </div>
-                            )
+                            console.log(node)
+                            return (<Case post={node} key={1}/>)
                         })}
                     </div>
                 )
